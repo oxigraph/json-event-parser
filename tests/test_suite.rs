e5a6e9a29dd5ea03e77bad9950e1cf7508dbd0fa
Tpt <thomaspt@hotmail.fr>
2022-01-04 22:55:12 +0100

    More tests, validate surrogates and allow BOM

--- tests/test_suite.rs
+++ tests/test_suite.rs
@@ -1,7 +1,48 @@
 use json_event_parser::{JsonEvent, JsonReader, JsonWriter};
 use std::fs::{read_dir, File};
 use std::io::{BufReader, Read, Result};
-use std::str;
+use std::{fs, str};
+
+const OTHER_VALID_TESTS: [&str; 12] = [
+    "i_number_double_huge_neg_exp.json",
+    "i_number_huge_exp.json",
+    "i_number_neg_int_huge_exp.json",
+    "i_number_pos_double_huge_exp.json",
+    "i_number_real_neg_overflow.json",
+    "i_number_real_pos_overflow.json",
+    "i_number_real_underflow.json",
+    "i_number_too_big_neg_int.json",
+    "i_number_too_big_pos_int.json",
+    "i_number_very_big_negative_int.json",
+    "i_structure_500_nested_arrays.json",
+    "i_structure_UTF-8_BOM_empty_object.json",
+];
+
+const OTHER_INVALID_TESTS: [&str; 23] = [
+    "i_object_key_lone_2nd_surrogate.json",
+    "i_string_1st_surrogate_but_2nd_missing.json",
+    "i_string_1st_valid_surrogate_2nd_invalid.json",
+    "i_string_incomplete_surrogate_and_escape_valid.json",
+    "i_string_incomplete_surrogate_pair.json",
+    "i_string_incomplete_surrogates_escape_valid.json",
+    "i_string_invalid_lonely_surrogate.json",
+    "i_string_invalid_surrogate.json",
+    "i_string_invalid_utf-8.json",
+    "i_string_inverted_surrogates_U+1D11E.json",
+    "i_string_iso_latin_1.json",
+    "i_string_lone_second_surrogate.json",
+    "i_string_lone_utf8_continuation_byte.json",
+    "i_string_not_in_unicode_range.json",
+    "i_string_overlong_sequence_2_bytes.json",
+    "i_string_overlong_sequence_6_bytes.json",
+    "i_string_overlong_sequence_6_bytes_null.json",
+    "i_string_truncated-utf-8.json",
+    "i_string_UTF8_surrogate_U+D800.json",
+    "i_string_utf16BE_no_BOM.json",
+    "i_string_utf16LE_no_BOM.json",
+    "i_string_UTF-8_invalid_sequence.json",
+    "i_string_UTF-16LE_with_BOM.json",
+];
 
 #[test]
 fn test_testsuite_parsing() -> Result<()> {
@@ -15,7 +56,7 @@ fn test_testsuite_parsing() -> Result<()> {
             continue;
         }
         let result = parse_result(File::open(file.path())?);
-        if file_name.starts_with("y_") {
+        if file_name.starts_with("y_") || OTHER_VALID_TESTS.contains(&file_name.as_ref()) {
             match result {
                 Ok(serialization) => match parse_result(serialization.as_slice()) {
                     Ok(other_serialization) => assert_eq!(serialization, other_serialization),
@@ -25,14 +66,21 @@ fn test_testsuite_parsing() -> Result<()> {
                         error
                     ),
                 },
-                Err(error) => panic!("Parsing of {} failed with error {}", file_name, error),
+                Err(error) => panic!(
+                    "Parsing of {} failed with error {} on {:x?}",
+                    file_name,
+                    error,
+                    fs::read(file.path())?
+                ),
             }
-        } else if file_name.starts_with("n_") {
-            assert!(
-                result.is_err(),
-                "Parsing of {} wrongly succeeded",
-                file_name
-            )
+        } else if file_name.starts_with("n_") || OTHER_INVALID_TESTS.contains(&file_name.as_ref()) {
+            if let Ok(json) = result {
+                panic!(
+                    "Parsing of {} wrongly succeeded with json {}",
+                    file_name,
+                    str::from_utf8(&json).unwrap()
+                )
+            }
         }
     }
     Ok(())
