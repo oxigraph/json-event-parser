34ce19208baf7ffe68f9e2f20ad7e394d51d55e2
Tpt <thomaspt@hotmail.fr>
2022-01-10 19:45:41 +0100

    Uses README for crate main documentation

--- README.md
+++ README.md
@@ -53,9 +53,9 @@ assert_eq!(buffer.as_slice(), b"{\"foo\":1}");
 This project is licensed under either of
 
  * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
-   http://www.apache.org/licenses/LICENSE-2.0)
+   `<http://www.apache.org/licenses/LICENSE-2.0>`)
  * MIT license ([LICENSE-MIT](LICENSE-MIT) or
-   http://opensource.org/licenses/MIT)
+   `<http://opensource.org/licenses/MIT>`)
    
 at your option.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,14 @@
-//! JSON event parser is a simple streaming JSON parser and serializer implementation.
-//!
-//! It does not aims to be the fastest or the more versatile JSON parser possible but to just be an as simple as possible implementation.
-//!
-//! Reader example:
-//!
-//! ```rust
-//! use json_event_parser::{JsonReader, JsonEvent};
-//! use std::io::Cursor;
-//!
-//! let json = b"{\"foo\": 1}";
-//! let mut reader = JsonReader::from_reader(Cursor::new(json));
-//!
-//! let mut buffer = Vec::new();
-//! assert_eq!(JsonEvent::StartObject, reader.read_event(&mut buffer)?);
-//! assert_eq!(JsonEvent::ObjectKey("foo"), reader.read_event(&mut buffer)?);
-//! assert_eq!(JsonEvent::Number("1"), reader.read_event(&mut buffer)?);
-//! assert_eq!(JsonEvent::EndObject, reader.read_event(&mut buffer)?);
-//! assert_eq!(JsonEvent::Eof, reader.read_event(&mut buffer)?);
-//!
-//! # std::io::Result::Ok(())
-//! ```
-//!
-//! Writer example:
-//!
-//! ```rust
-//! use json_event_parser::{JsonWriter, JsonEvent};
-//!
-//! let mut buffer = Vec::new();
-//! let mut writer = JsonWriter::from_writer(&mut buffer);
-//! writer.write_event(JsonEvent::StartObject)?;
-//! writer.write_event(JsonEvent::ObjectKey("foo"))?;
-//! writer.write_event(JsonEvent::Number("1"))?;
-//! writer.write_event(JsonEvent::EndObject)?;
-//!
-//! assert_eq!(buffer.as_slice(), b"{\"foo\":1}");
-//!
-//! # std::io::Result::Ok(())
-//! ```
+#![doc = include_str!("../README.md")]
+#![deny(
+    future_incompatible,
+    nonstandard_style,
+    rust_2018_idioms,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused_qualifications
+)]
 
 mod read;
 mod write;
