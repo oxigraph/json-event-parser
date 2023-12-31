98ed4e6261a7972c869a83de487c7a4ade2be0bd
Tpt <thomaspt@hotmail.fr>
2023-12-31 16:30:50 +0100

    Integrates codespeed and improves benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -1,8 +1,26 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 use json_event_parser::{FromReadJsonReader, JsonEvent};
 use std::fs::{self, read_dir};
 
-fn bench_json_parse(c: &mut Criterion) {
+fn bench_parse_json_benchmark(c: &mut Criterion) {
+    for dataset in ["canada", "citm_catalog", "twitter"] {
+        let data = fs::read(format!(
+            "{}/benches/json-benchmark/data/{dataset}.json",
+            env!("CARGO_MANIFEST_DIR")
+        ))
+        .unwrap();
+        c.bench_function(dataset, |b| {
+            b.iter(|| {
+                let mut reader = FromReadJsonReader::new(data.as_slice());
+                while reader.read_next_event().unwrap() != JsonEvent::Eof {
+                    //read more
+                }
+            })
+        });
+    }
+}
+
+fn bench_parse_testsuite(c: &mut Criterion) {
     let example = load_testsuite_example();
 
     c.bench_function("JSON test suite", |b| {
@@ -11,16 +29,6 @@ fn bench_json_parse(c: &mut Criterion) {
             while reader.read_next_event().unwrap() != JsonEvent::Eof {
                 //read more
             }
-        })
-    });
-}
-
-fn bench_serde_json_parse(c: &mut Criterion) {
-    let example = load_testsuite_example();
-
-    c.bench_function("Serde JSON test suite", |b| {
-        b.iter(|| {
-            let _: serde_json::Value = serde_json::from_reader(example.as_slice()).unwrap();
         })
     });
 }
@@ -48,6 +56,6 @@ fn load_testsuite_example() -> Vec<u8> {
     result
 }
 
-criterion_group!(parser, bench_json_parse, bench_serde_json_parse);
+criterion_group!(parser, bench_parse_testsuite, bench_parse_json_benchmark);
 
 criterion_main!(parser);
