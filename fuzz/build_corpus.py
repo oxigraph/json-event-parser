c173102ce45c03b316aa0d7e5b8509027cde72c8
Thomas Tanon <thomas@pellissier-tanon.fr>
2024-10-18 19:53:45 +0200

    CI: better fuzzing script

--- fuzz/build_corpus.py
+++ fuzz/build_corpus.py
@@ -5,7 +5,7 @@ from pathlib import Path
 base = Path(__file__).parent.parent
 target_dir = base / "fuzz" / "corpus" / "parse"
 target_dir.mkdir(parents=True, exist_ok=True)
-for f in base.rglob("*.py"):
+for f in base.rglob("*.json"):
     for _ in range(3):
         data = f.read_bytes()
         pos = random.randint(0, len(data))
